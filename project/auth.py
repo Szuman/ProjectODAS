04c5b263b414c68b30a047fa2f78280eda21c4a6
Szuman <piszuman@gmail.com>
2022-01-23 19:14:30 +0100

    Hashowanie hasła za pomocą bcrypt

--- project/auth.py
+++ project/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
-from werkzeug.security import generate_password_hash, check_password_hash
+import passlib.hash
 from flask_login import login_user, login_required, logout_user
 from .models import User
 from . import db
@@ -12,19 +12,15 @@ def login():
 
 @auth.route('/login', methods=['POST'])
 def login_post():
-    # login code goes here
     email = request.form.get('email')
     password = request.form.get('password')
 
     user = User.query.filter_by(email=email).first()
 
-    # check if the user actually exists
-    # take the user-supplied password, hash it, and compare it to the hashed password in the database
-    if not user or not check_password_hash(user.password, password):
+    if not passlib.hash.bcrypt.verify(password, user.password):
         flash('Please check your login details and try again.')
-        return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
+        return redirect(url_for('auth.login'))
 
-    # if the above check passes, then we know the user has the right credentials
     login_user(user)
     return redirect(url_for('main.profile'))
 
@@ -34,21 +30,18 @@ def signup():
 
 @auth.route('/signup', methods=['POST'])
 def signup_post():
-    # code to validate and add user to database goes here
     email = request.form.get('email')
     name = request.form.get('name')
     password = request.form.get('password')
 
-    user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
+    user = User.query.filter_by(email=email).first()
 
-    if user: # if a user is found, we want to redirect back to signup page so user can try again
+    if user:
         flash('Email address already exists')
         return redirect(url_for('auth.signup'))
 
-    # create a new user with the form data. Hash the password so the plaintext version isn't saved.
-    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+    new_user = User(email=email, name=name, password=passlib.hash.bcrypt.using(rounds=16, salt='1234567890098765432112').hash(password))
 
-    # add the new user to the database
     db.session.add(new_user)
     db.session.commit()
     return redirect(url_for('auth.login'))
